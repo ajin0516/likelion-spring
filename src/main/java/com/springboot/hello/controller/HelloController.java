f5ffaf49436c003477c8bd9661b003b7f06113e6
ajin0516 <dkwls7432@naver.com>
2022-10-27 13:20:27 +0900

    RequestParam MemberDto 적용

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.springboot.hello.controller;
 
+import com.springboot.hello.domain.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController // 이게 붙어야 spring이 controller로 인식, dispatcherServlet이 매핑해줄 controller를 등록
 @RequestMapping("/api/v1/get-api") // controller가 할당 된 후 어떤 method를 실행할지 연결해주는 어노테이션
@@ -21,9 +24,20 @@ public class HelloController {
     }
 
     @GetMapping(value = "/request1")
-    public String getVariable2(@RequestParam String name, @RequestParam String email, @RequestParam String organization) {
+    public String getVariable1(@RequestParam String name, @RequestParam String email, @RequestParam String organization) {
 
         return String.format("%s %s %s ", name, email, organization);
     }
 
+    @GetMapping(value = "/request2")
+    public String getVariable2(@RequestParam Map<String, String> param){
+        param.entrySet().forEach((map)->{
+            System.out.printf("key : %s ,value : %s\n", map.getKey(), map.getValue());
+        });
+        return "request2가 호출 완료 되었습니다";
+    }
+    @GetMapping(value = "/request3")
+    public String getRequestParam(MemberDto memberDto){
+        return memberDto.toString();
+    }
 }
