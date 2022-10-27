fd4582e59f40a14bb21fc06b3ab4415923f89772
ajin0516 <dkwls7432@naver.com>
2022-10-27 14:38:58 +0900

    [spring] Put

--- src/main/java/com/springboot/hello/controller/PutController.java
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -1,14 +1,16 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Objects;
 
 @RestController
-@RequestMapping("/api/v1/post-api")
-public class PostController {
+@RequestMapping("/api/v1/put-api")
+public class PutController {
 
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample(){
@@ -26,6 +28,13 @@ public class PostController {
     @PostMapping ("/member2")
     public String postMember(@RequestBody MemberDto memberDto){
         return memberDto.toString();
+    }
+
+    @PutMapping ("/member3")
+    public ResponseEntity<MemberDto> putMember (@RequestBody MemberDto memberDto){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
     }
 
 }
