bdd4fbb946a8b640e25b651ecf4ec45574ee650c
ajin0516 <dkwls7432@naver.com>
2022-10-28 14:45:24 +0900

    [CRUD] User, UserDao 작성

--- src/main/java/com/springboot/hello/dao/UserDao.java
+++ src/main/java/com/springboot/hello/dao/UserDao.java
@@ -1,14 +1,47 @@
 package com.springboot.hello.dao;
 
+import com.springboot.hello.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
 
 @Component
 public class UserDao {
 
+    private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate;
 
-    public UserDao(JdbcTemplate jdbcTemplate) {
+    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public void add(User user){
+        this.jdbcTemplate.update("INSERT INTO last_db(id, name, password) VALUES (?,?,?)",
+        user.getId(), user.getName(), user.getPassword());
+    }
+
+    public int deleteAll(){
+        return this.jdbcTemplate.update("DELETE FROM last_db");
+    }
+
+    public User findById(String id) {
+    String sql = "select * from users where id = ?";
+        RowMapper<User> rowMapper = new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("passsword"));
+                return user;
+            }
+        };
+        return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 }
